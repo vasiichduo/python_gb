152c15053a66fb68f6d23bc7a48ef8de820b4b36
vasiichduo <new.x.pro@gmail.com>
2021-07-19 23:26:17 -0700

    Delete less_1 directory

--- less_1/task_1_1.py
+++
@@ -1,5 +0,0 @@
-duration = int(input("Продолжительность секунд:"))
-minutes=duration//60             # минуты
-hours=duration//3600             # часы
-days=duration//86400             # дни
-print (days%30, "день", hours%24, "час", minutes%60, "мин", duration%60, "сек")
