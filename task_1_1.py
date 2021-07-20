8ef3ccea240b0cdb0dc33583486d461aedbb3ff2
vasiichduo <new.x.pro@gmail.com>
2021-07-20 16:36:58 +0300

    Update task_1_1.py

--- task_1_1.py
+++ task_1_1.py
@@ -2,4 +2,4 @@ duration = int(input("Продолжительность секунд:"))
 minutes=duration//60             # минуты
 hours=duration//3600             # часы
 days=duration//86400             # дни
-print (days%30, "день", hours%24, "час", minutes%60, "мин", duration%60, "сек")
+print (days, "день", hours%24, "час", minutes%60, "мин", duration%60, "сек")
